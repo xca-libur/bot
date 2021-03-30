33a5ac61e3b3b4916340ec1eca1dc5af7da52cdf
xca-libur <79561225+xca-libur@users.noreply.github.com>
2021-03-30 15:56:37 +0600

    Update premium.ts

--- src/inhibitors/premium.ts
+++ src/inhibitors/premium.ts
@@ -93,7 +93,9 @@ export default class PremiumInhibitor extends Inhibitor {
       const members = await guild.members.fetch().catch(() => {});
       if (!members) return;
       for (const [, member] of members)
-        if (paidIds.includes(member.id))
+        if (member.roles.cache.has(role.id) && removeIds.includes(member.id) && !this.client.config.dev)
+          await member.roles.remove(role, "premium is gone :crabrave:");
+        else if (paidIds.includes(member.id))
           await member.roles.add(role, "wow member now has premium");
     });
   }
